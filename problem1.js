842fb2836f5d878e934ceb147438c2b6ef74829d
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-09-16 14:33:25 +0530

    initial commit for problem 1

---
+++ problem1.js
@@ -0,0 +1,62 @@
+/*
+    Problem 1:
+    
+    Using callbacks and the fs module's asynchronous functions, do the following:
+        1. Create a directory of random JSON files
+        2. Delete those files simultaneously 
+*/
+
+const fs = require('fs');
+
+const createAndDeleteFiles = (cb) => {
+  createDirectory((error, data) => {
+    if (error) {
+      cb(error);
+    } else {
+      cb(null, data);
+      createFiles((error, data) => {
+        if (error) {
+          cb(error);
+        } else {
+          cb(null, data);
+        }
+      });
+    }
+  });
+};
+
+const createDirectory = (callback) => {
+  fs.mkdir('./newDirectory', { recursive: true }, (error) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, 'Directory created!');
+    }
+  });
+};
+
+const createFiles = (callback) => {
+  for (let i = 0; i < Math.floor(Math.random() * 500); i++) {
+    const fileName = './newDirectory/random' + i + '.json';
+    fs.writeFile(fileName, '', (error) => {
+      if (error) {
+        callback(error);
+      } else {
+        callback(null, `${fileName} is created!`);
+        deleteFiles(fileName, callback);
+      }
+    });
+  }
+};
+
+const deleteFiles = (fileName, callback) => {
+  fs.unlink(fileName, (error) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, `${fileName} 'is deleted!`);
+    }
+  });
+};
+
+module.exports = createAndDeleteFiles;
